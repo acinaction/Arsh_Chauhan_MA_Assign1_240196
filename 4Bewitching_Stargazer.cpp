293247c6f526a81d6fdf169aebafcc53219dce7d
acinaction <chauhanarsh1312@gmail.com>
2025-05-21 01:07:36 +0530

    Problem 4

---
+++ 4Bewitching_Stargazer.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long doit(int s,int e, int k){
+    if(e-s+1<k) return 0;
+    int m=s+(e-s)/2;
+    return (e-s+1)%2==0?doit(s,m,k) + doit(m+1,e,k):m+doit(s,m-1,k) + doit(m+1,e,k);
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+    cin>>n>>k;
+    long long lucky_value=doit(1,n,k);
+    cout<<lucky_value<<endl;
+    }
+}
