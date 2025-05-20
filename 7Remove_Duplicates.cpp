03123b6a36041f7927f5a5b280a407ae553155bc
acinaction <chauhanarsh1312@gmail.com>
2025-05-21 01:09:02 +0530

    Problem 7

---
+++ 7Remove_Duplicates.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        auto it = nums.begin();
+        // while(it!=nums.end()){
+        //     if(it==nums.begin()){
+        //         it++;
+        //     }
+        //     else if((*it)==(*(it-1))){
+        //         nums.erase(it);
+        //     }
+        //     else{
+        //         it++;
+        //     }
+        // }
+
+
+        // while(it!=nums.end()){
+        //     int a = *it;
+        //     it++;
+        //     while(it!=nums.end() && (*it)==a){
+        //         nums.erase(it);
+        //     }
+        // }
+
+        while(it!=nums.end()){
+            int a = *it;
+            it++;
+            while(it!=upper_bound(nums.begin(),nums.end(),a)){
+                nums.erase(it);
+            }
+        }
+        return nums.size();
+    }
+};
