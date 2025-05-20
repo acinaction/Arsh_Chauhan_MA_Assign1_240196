28d240693e2cbfd07009efcabae20fffb2c00124
acinaction <chauhanarsh1312@gmail.com>
2025-05-21 01:07:14 +0530

    Problem 3

---
+++ 3Nth_root_of_m.cpp
@@ -0,0 +1,14 @@
+int NthRoot(int n, int m) {
+  int i; // I'll go from 1 to m and check whether the nth power equals m.
+  int flag=0;
+  for(i=1;i<=m;i++){
+    if(pow(i,n)==m){ // The case when i's nth power is m 
+      flag=1;
+      break;
+    }
+    else if(pow(i,n)<m && pow(i+1,n)>m){ // Ahead of this i, all i's will have nth power greater than m. Hence no solution possible. 
+      break;
+    }
+  }
+  return flag==1?i:-1;
+}
